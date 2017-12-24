ae491e63d3af215f6921eec15527b7c5752eda6b
asam139 <sam6.13.93@hotmail.com>
2017-12-24 13:13:30 +0100

    Create VAO, VBO & program to draw the first geometry --> a triangle!!!

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,104 @@ void handleInput(GLFWwindow* window) {
     }
 }
 
-void render() {
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+void render(uint32_t VAO, uint32_t shader) {
+    glClearColor(0.6f, 0.f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
+
+uint32_t createVertexData(uint32_t* VBO) {
+    float vertices[] = {
+            -0.5f, -0.5f, 0.f,
+            0.5f, -0.5f, 0.f,
+            0.f, 0.5f, 0.f
+    };
+
+    uint32_t VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    return VAO;
+}
+
+bool checkShader(uint32_t shader) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "Error Compiling Shader" << std::endl << infoLog << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool checkProgram(uint32_t program) {
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cout << "Error Linking Program" << std::endl << infoLog << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+uint32_t createProgram() {
+    const char *vertexShaderSource =
+            "#version 330core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "void main() {\n"
+            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.f);\n"
+            "}\0";
+
+    const char *fragmentShaderSource =
+            "#version 330core\n"
+            "out vec4 fragColor;\n"
+            "void main() {\n"
+            "   fragColor = vec4(0.2f, 0.7f, 0.2f, 1.f);\n"
+            "}\0";
+
+    uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkShader(vertexShader);
+
+    uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkShader(fragmentShader);
+
+    uint32_t shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
 
 int main (int argc, char *argv[]) {
 
@@ -47,16 +141,24 @@ int main (int argc, char *argv[]) {
 
     glfwSetFramebufferSizeCallback(window, &onChangeFramebufferSize);
 
+
+    uint32_t VBO;
+    uint32_t VAO = createVertexData(&VBO);
+    uint32_t shader = createProgram();
+
     while (!glfwWindowShouldClose(window)) { //Loop until user closes the window
         // Handle Input
         handleInput(window);
         //Render Here
-        render();
+        render(VAO, shader);
         //Swap front and back buffers
         glfwSwapBuffers(window);
         // Poll for and process events
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
