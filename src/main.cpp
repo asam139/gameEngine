e685ec880dda31442fbeb5319fd1e824017461a9
asam139 <sam6.13.93@hotmail.com>
2017-12-24 13:28:37 +0100

    Draw square as elements: reduce the number of vertices

--- src/main.cpp
+++ src/main.cpp
@@ -19,28 +19,34 @@ void render(uint32_t VAO, uint32_t shader) {
 
     glUseProgram(shader);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
-uint32_t createVertexData(uint32_t* VBO) {
+uint32_t createVertexData(uint32_t* VBO, uint32_t* EBO) {
     float vertices[] = {
             0.5f, 0.5f, 0.f,
-            0.5f, -0.5f, 0.f,
-            -0.5f, 0.5f, 0.f,
-
             0.5f, -0.5f, 0.f,
             -0.5f, -0.5f, 0.f,
             -0.5f, 0.5f, 0.f
     };
 
+    uint32_t indices[] = {
+            0, 3, 1,
+            1, 3, 2
+    };
+
     uint32_t VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, VBO);
+    glGenBuffers(1, EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, *VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
@@ -48,6 +54,8 @@ uint32_t createVertexData(uint32_t* VBO) {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     return VAO;
 }
@@ -146,8 +154,8 @@ int main (int argc, char *argv[]) {
     glfwSetFramebufferSizeCallback(window, &onChangeFramebufferSize);
 
 
-    uint32_t VBO;
-    uint32_t VAO = createVertexData(&VBO);
+    uint32_t VBO, EVO;
+    uint32_t VAO = createVertexData(&VBO, &EVO);
     uint32_t shader = createProgram();
 
     while (!glfwWindowShouldClose(window)) { //Loop until user closes the window
@@ -163,6 +171,7 @@ int main (int argc, char *argv[]) {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EVO);
 
     glfwTerminate();
     return 0;
