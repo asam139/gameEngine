6a8e3938debfd126b8964731994fee27347e7251
asam139 <sam6.13.93@hotmail.com>
2017-12-27 20:33:27 +0100

    The attribute size always is 3

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ uint32_t createVertexData(uint32_t* VBO, float vertices[], uint size) {
     glBindBuffer(GL_ARRAY_BUFFER, *VBO);
     glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, size/3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
