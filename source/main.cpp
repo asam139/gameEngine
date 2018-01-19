4e890652a4e164bea14d92eb98a8738906260453
asam139 <sam6.13.93@hotmail.com>
2018-01-19 18:19:54 +0100

    Form a X structure

--- source/main.cpp
+++ source/main.cpp
@@ -33,17 +33,21 @@ float lastX = (float)kScreenWidth / 2.f;
 float lastY = (float)kScreenHeight / 2.f;
 
 // Objects
+const unsigned int cubesCount = 9;
 glm::vec3 cubePositions[] = {
+        glm::vec3(-2.0f, 2.0f, 0.0f),
+        glm::vec3(-1.0f, 1.0f, 0.0f),
+
+        glm::vec3(2.0f, 2.0f, 0.0f),
+        glm::vec3(1.0f, 1.0f, 0.0f),
+
+        glm::vec3(-2.0f, -2.0f, 0.0f),
+        glm::vec3(-1.0f, -1.0f, 0.0f),
+
+        glm::vec3(2.0f, -2.0f, 0.0f),
+        glm::vec3(1.0f, -1.0f, 0.0f),
+
         glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(2.0f, 5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f, 3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f, 2.0f, -2.5f),
-        glm::vec3(1.5f, 0.2f, -1.5f),
-        glm::vec3(-1.3f, 1.0f, -1.5f)
 };
 
 ///////////////////////
@@ -119,8 +123,7 @@ void render(const Cube& cube, const Shader& shader, const GLuint text0) {
     shader.set("projection", projection);
 
     bool first = true;
-
-    for (uint8_t i = 0; i < 10; i++) {
+    for (uint8_t i = 0; i < cubesCount; i++) {
         glm::mat4 model = glm::mat4(1.0f);
 
 
