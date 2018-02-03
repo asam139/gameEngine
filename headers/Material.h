e743e28ad2c0698132591ef93c4b9a57bc5bd4eb
asam139 <sam6.13.93@hotmail.com>
2018-02-03 18:46:11 +0100

    Material class: clean code

--- headers/Material.h
+++ headers/Material.h
@@ -19,18 +19,14 @@ public:
 
     void setDiffuseColor(glm::vec3 diffuseColor);
     void setDiffuseTexture(std::shared_ptr<Texture> texture);
-    void setDiffuseText(uint32_t diffuseText);
     glm::vec3 getDiffuseColor();
     Texture* getDiffuseTexture();
-    uint32_t getDiffuseText();
 
 
     void setSpecularColor(glm::vec3 specularColor);
     void setSpecularTexture(std::shared_ptr<Texture> texture);
-    void setSpecularText(uint32_t specularText);
     glm::vec3 getSpecularColor();
     Texture* getSpecularTexture();
-    uint32_t getSpecularText();
     float getShininess();
     void setShininess(float shininess);
 
@@ -38,11 +34,9 @@ public:
     void setEmissionActive(bool emissionActive);
     void setEmissiveColor(glm::vec3 emissiveColor);
     void setEmissiveTexture(std::shared_ptr<Texture> texture);
-    void setEmissiveText(uint32_t emissiveText);
     bool getEmissionActive();
     glm::vec3 getEmissiveColor();
     Texture* getEmissiveTexture();
-    uint32_t getEmissiveText();
 
 
     Shader* getShader();
@@ -53,17 +47,14 @@ protected:
 
     glm::vec3 _diffuseColor;
     std::shared_ptr<Texture> _diffuseTexture;
-    uint32_t _diffuseText;
 
     glm::vec3 _specularColor;
     std::shared_ptr<Texture> _specularTexture;
-    uint32_t _specularText;
     float _shininess;
 
     bool _emissionActive;
     glm::vec3 _emissiveColor;
     std::shared_ptr<Texture> _emissiveTexture;
-    uint32_t _emissiveText;
 
     std::shared_ptr<Shader> _shader;
 private:
--- source/Material.cpp
+++ source/Material.cpp
@@ -13,18 +13,15 @@ Material::Material(std::shared_ptr<Shader> shader) {
 
     // Diffuse
     setDiffuseColor(glm::vec3(1.0f));
-    setDiffuseText(0);
     _diffuseTexture = nullptr;
 
     // Specular
     setSpecularColor(glm::vec3(0.25f));
-    setSpecularText(0);
     _specularTexture = nullptr;
     setShininess(32.f);
 
     // Emissive
     setEmissiveColor(glm::vec3(0.0f));
-    setEmissiveText(0);
     _emissiveTexture = nullptr;
 }
 
@@ -59,14 +56,6 @@ Texture* Material::getDiffuseTexture() {
     return _diffuseTexture.get();
 }
 
-void Material::setDiffuseText(uint32_t diffuseText) {
-    _diffuseText = diffuseText;
-}
-
-uint32_t Material::getDiffuseText() {
-    return _diffuseText;
-}
-
 // Specular
 void Material::setSpecularColor(glm::vec3 specularColor) {
     _specularColor = specularColor;
@@ -84,15 +73,6 @@ Texture* Material::getSpecularTexture() {
     return _specularTexture.get();
 }
 
-
-void Material::setSpecularText(uint32_t specularText) {
-    _specularText = specularText;
-}
-
-uint32_t Material::getSpecularText() {
-    return _specularText;
-}
-
 void Material::setShininess(float shininess) {
     _shininess = shininess;
 }
@@ -107,7 +87,6 @@ void Material::setEmissiveColor(glm::vec3 emissiveColor) {
     _emissiveColor = emissiveColor;
 }
 
-
 glm::vec3 Material::getEmissiveColor() {
     return _emissiveColor;
 }
@@ -118,14 +97,6 @@ void Material::setEmissiveTexture(std::shared_ptr<Texture> texture) {
 
 Texture* Material::getEmissiveTexture() {
     return _emissiveTexture.get();
-}
-
-void Material::setEmissiveText(uint32_t emissiveText) {
-    _emissiveText = emissiveText;
-}
-
-uint32_t Material::getEmissiveText() {
-    return _emissiveText;
 }
 
 void Material::setEmissionActive(bool emissionActive) {
@@ -148,18 +119,16 @@ void Material::configureShader() {
     // Diffuse
     _shader->set("material.diffuse_color", _diffuseColor);
     if (_diffuseTexture != nullptr) {
-        setDiffuseText(0);
         _diffuseTexture->activeTextureAs(GL_TEXTURE0);
+        _shader->set("material.specular_text", 1);
     }
-    _shader->set("material.diffuse_text", 0);
 
     // Specular
     _shader->set("material.specular_color", _specularColor);
     if (_specularTexture != nullptr) {
-        setSpecularText(1);
         _specularTexture->activeTextureAs(GL_TEXTURE1);
+        _shader->set("material.specular_text", 1);
     }
-    _shader->set("material.specular_text", 1);
 
     _shader->set("material.shininess", _shininess);
 
@@ -168,10 +137,9 @@ void Material::configureShader() {
     if (_emissionActive) {
         _shader->set("material.emissive_color", _emissiveColor);
         if (_emissiveTexture != nullptr) {
-            setEmissiveText(2);
             _emissiveTexture->activeTextureAs(GL_TEXTURE2);
+            _shader->set("material.emissive_text", 2);
         }
-        _shader->set("material.emissive_text", 2);
     }
 
     // Set to use
